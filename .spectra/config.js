4071011fae3fe10b61287f0298d3166976a03b84
sergeykitchen <kitchenkov84@gmail.com>
2021-01-20 07:38:55 +0200

    added pahe index to navigation

--- .spectra/config.js
+++ .spectra/config.js
@@ -1,13 +1,14 @@
-const markdownIt = require.main.require("markdown-it");
+const markdownIt = require.main.require('markdown-it');
 const markdownItReplaceLink = require.main.require('markdown-it-replace-link');
-const { mathpixMarkdownPlugin, initMathpixMarkdown } = require.main.require('mathpix-markdown-it');
-const eleventyPluginFilesMinifier = require.main.require("@sherby/eleventy-plugin-files-minifier");
+const { mathpixMarkdownPlugin, initMathpixMarkdown } = require.main.require(
+  'mathpix-markdown-it'
+);
+const eleventyPluginFilesMinifier = require.main.require(
+  '@sherby/eleventy-plugin-files-minifier'
+);
 // const {removeExt } = require.main.require("../lib/utils/filters/");
 const util = require('util');
 const navigation = require('./navigation.json');
-
-
-
 
 const DEV_MODE = process.env.ELEVENTY_ENV === 'dev';
 
@@ -21,9 +22,9 @@ const getMmdOptions = () => {
     openLinkInNewWindow: false,
     smiles: {
       disableColors: false,
-      disableGradient: false
-    }
-  }
+      disableGradient: false,
+    },
+  };
 };
 
 module.exports = function (config) {
@@ -35,27 +36,33 @@ module.exports = function (config) {
   //   return value.replace(/\.[^/.]+$/, "").toLowerCase();
   // });
 
-  config.addCollection('pages', collection => {
-    return collection.getAllSorted().map((item) => {
-      item.outputPath = item.outputPath.toLowerCase();
-      item.lowerURL = item.url.toLowerCase();
-      item.data.page.url = item.lowerURL;
-      return item
-    }).sort((a, b) => {
-      return a.lowerURL.localeCompare(b.lowerURL)
-    });
+  config.addCollection('pages', (collection) => {
+    return collection
+      .getAllSorted()
+      .map((item) => {
+        item.outputPath = item.outputPath.toLowerCase();
+        item.lowerURL = item.url.toLowerCase();
+        item.data.page.url = item.lowerURL;
+        return item;
+      })
+      .sort((a, b) => {
+        return a.lowerURL.localeCompare(b.lowerURL);
+      });
   });
 
-  config.addCollection('navigation', ()=> {
+  config.addCollection('navigation', () => {
     return navigation;
   });
 
   if (DEV_MODE) {
     const sassPluginOptions = {
       autoprefixer: true,
-      outputDir: './assets/css'
-    }
-    config.addPlugin(require.main.require("eleventy-plugin-sass"), sassPluginOptions);
+      outputDir: './assets/css',
+    };
+    config.addPlugin(
+      require.main.require('eleventy-plugin-sass'),
+      sassPluginOptions
+    );
     config.addPassthroughCopy('./assets/css');
     config.addPassthroughCopy('./assets/scripts');
   }
@@ -64,17 +71,21 @@ module.exports = function (config) {
     breaks: true,
     linkify: true,
     replaceLink(link, env) {
-
-      link = link.toLowerCase()
+      link = link.toLowerCase();
 
       // Skip outbound link.
       let isOutbound = new RegExp('^(?:[a-z]+:)?//', 'i');
-      if (isOutbound.test(link)) { return link }
+      if (isOutbound.test(link)) {
+        return link;
+      }
 
       // Remove markdown extension.
-      let isMarkdown = new RegExp('(?:(index))?.(md|mkd|mkdn|mdwn|mdown|markdown|mdl|mmd)$', 'i');
+      let isMarkdown = new RegExp(
+        '(?:(index))?.(md|mkd|mkdn|mdwn|mdown|markdown|mdl|mmd)$',
+        'i'
+      );
       if (isMarkdown.test(link)) {
-        link = link.replace(isMarkdown, "");
+        link = link.replace(isMarkdown, '');
       }
 
       // Check if there's an extension.
@@ -87,60 +98,56 @@ module.exports = function (config) {
       // Ignore absolute root link.
       let isAbsolute = new RegExp(`^\/$`);
       if (isAbsolute.test(link)) {
-        return link
+        return link;
       }
 
-      let isInsideIndex = new RegExp('(?:(index)).(md|mkd|mkdn|mdwn|mdown|markdown|mdl|mmd)$', 'i');
+      let isInsideIndex = new RegExp(
+        '(?:(index)).(md|mkd|mkdn|mdwn|mdown|markdown|mdl|mmd)$',
+        'i'
+      );
       if (isInsideIndex.test(env.page.inputPath)) {
         // Fix links inside INDEX.md or index.md files.
-        return config.getFilter("url")(link);
+        return config.getFilter('url')(link);
       } else {
         // Fix relative links.
-        return config.getFilter("url")(`../${link}`);
+        return config.getFilter('url')(`../${link}`);
       }
-    }
+    },
   };
 
-  config.addFilter(
-    "relative",
-    (page, root = "/") => {
-      return `${require('path').relative(page.filePathStem, root)}/`
+  config.addFilter('relative', (page, root = '/') => {
+    console.log('page', page);
+    if (page.inputPath == './index.md') {
+      return './';
     }
-  );
+    return `${require('path').relative(page.filePathStem, root)}/`;
+  });
 
-  let md = markdownIt(markdownItOptions)
-  md.use(markdownItReplaceLink)
+  let md = markdownIt(markdownItOptions);
+  md.use(markdownItReplaceLink);
   md = initMathpixMarkdown(md, getMmdOptions);
   md.use(mathpixMarkdownPlugin, {});
 
-  config.setTemplateFormats([
-    "md",
-    "mmd",
-    "png",
-    "gif",
-    "jpg",
-    "jpeg",
-    "pdf",
-  ]);
+  config.setTemplateFormats(['md', 'mmd', 'png', 'gif', 'jpg', 'jpeg', 'pdf']);
 
-  config.setLibrary("md", md);
-  config.setLibrary("mmd", md);
+  config.setLibrary('md', md);
+  config.setLibrary('mmd', md);
 
   config.setBrowserSyncConfig({
-    logLevel: "info",
-    logPrefix: "Spectra",
+    logLevel: 'info',
+    logPrefix: 'Spectra',
     logFileChanges: false,
     ui: false,
-    notify: true
+    notify: true,
   });
 
   return {
-    pathPrefix: "/",
+    pathPrefix: '/',
     dir: {
       includes: '.spectra/includes',
       layouts: '.spectra/layout',
-      data: '.spectra/layout'
+      data: '.spectra/layout',
     },
-    templateFormats: ['md', 'mmd']
+    templateFormats: ['md', 'mmd'],
   };
 };
